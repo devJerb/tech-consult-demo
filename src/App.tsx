ea9aa2aa198528e15c5b1df84d0889ef4a4ecc25
devJerb <gutierrez.jeromebryant@gmail.com>
2024-12-06 11:00:50 +0800

    chore: separate gridline tw

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import ChatbotWidget from "./components/section/chatbot";
 
 const App = () => {
+  const gridLines =
+    "absolute inset-0 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]";
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 absolute inset-0 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+    <main
+      className={`flex min-h-screen flex-col items-center justify-center p-24 ${gridLines}`}
+    >
       <h1 className="text-4xl font-bold mb-8">Welcome to Method IT</h1>
       <p className="text-xl mb-4">How can we assist you today?</p>
       <ChatbotWidget />
